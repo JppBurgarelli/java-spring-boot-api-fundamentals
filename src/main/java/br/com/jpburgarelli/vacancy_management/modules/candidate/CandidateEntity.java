19df37aff0fd80f12aaaa3428a98b7e036e729e7
joao burgarelli jp <jp.burgarelli@gmail.com>
2024-12-18 11:23:38 -0300

    feat: add candidate controler & entity

---
+++ src/main/java/br/com/jpburgarelli/vacancy_management/modules/candidate/CandidateEntity.java
@@ -0,0 +1,17 @@
+package br.com.jpburgarelli.vacancy_management.modules.candidate;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+  private UUID id;
+  private String name;
+  private String username;
+  private String email;
+  private String password;
+  private String description;
+  private String resume;
+  
+}
