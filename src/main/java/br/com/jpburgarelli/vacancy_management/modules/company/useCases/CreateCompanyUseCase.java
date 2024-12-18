bb029921ef8d0f5ff2d76c71c2b4f62a268066ec
joao burgarelli jp <jp.burgarelli@gmail.com>
2024-12-18 19:15:57 -0300

    feat: add useCase & company controller

--- src/main/java/br/com/jpburgarelli/vacancy_management/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/jpburgarelli/vacancy_management/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,4 +1,4 @@
-package br.com.jpburgarelli.vacancy_management.modules.company.entities.useCases;
+package br.com.jpburgarelli.vacancy_management.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
